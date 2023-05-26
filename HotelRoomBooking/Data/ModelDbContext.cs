88742fbbeb5ef973fde515ebe1b3729ccb7097f1
sathyanRavikumar <134672524+sathyanRavikumar@users.noreply.github.com>
2023-05-26 12:43:26 +0530

    Model and Migration

---
+++ HotelRoomBooking/Data/ModelDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HotelRoomBooking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelRoomBooking.Data
+{
+    public class ModelDbContext : DbContext
+    {
+        public ModelDbContext(DbContextOptions<ModelDbContext> options) : base(options)
+        {
+
+        }
+
+        
+        public DbSet<Hotels> hotels { get; set; }
+        public DbSet<Resevation> resevations { get; set; }
+        public DbSet<Rooms> rooms { get; set; }
+        public DbSet<User> users { get; set; }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+    }
+}
---
+++ HotelRoomBooking/Models/Resevation.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelRoomBooking.Models
+{
+    public class Resevation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int reserId { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int userId { get; set; }
+
+        [ForeignKey(nameof(Rooms))]
+        public int RoomId { get; set; }
+
+        public string? Cust_address { get; set; }
+        public DateFormat checkIn { get; set; }
+        public DateFormat checkOut { get; set; }
+
+
+
+    }
+}
